a600229d1cc378d236625a25e6bf28dd7761c7a3
zeribr <rondillazerib@gmail.com>
2026-03-18 23:16:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -262,8 +262,6 @@ with button_col:
                 )
                 
                 if res.status_code == 200:
-                    st.success("Cleared!")
-                    time.sleep(0.5)
                     st.rerun()
                 else:
                     # Specific error if the API key is wrong
